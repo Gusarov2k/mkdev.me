08e0495802f5711559965e7cdf8fecb4b9fbbd4e
Viktor Kanaev <v.kanaev@potok.digital>
2020-01-10 15:53:16 +0300

    Codereview based fixes

--- movies.rb
+++ movies.rb
@@ -2,22 +2,19 @@ require 'date'
 
 file_name = ARGV.first || 'movies.txt'
 file = File.open file_name
-movies = []
 
 def date_safe_parse(str)
-  patern =  if str.slice(/\d{4}-\d{2}-\d{2}/)
-              '%Y-%m-%d'
-            elsif str.slice(/\d{4}-\d{2}/)
-              '%Y-%m'
-            else
-              '%Y'
-            end
+  patern = case str
+           when /\d{4}-\d{2}-\d{2}/ then '%Y-%m-%d'
+           when /\d{4}-\d{2}/       then '%Y-%m'
+           else                          '%Y'
+           end
   Date.strptime(str, patern)
 end
 
-file.each do |line|
+movies = file.map do |line|
   arr = line.split('|')
-  movies << {
+  {
     imdb_link: arr[0],
     title: arr[1],
     year: arr[2].to_i,
@@ -36,14 +33,13 @@ def movie_format(obj)
 end
 
 puts '# 5 movies with maximum duration:'
-movies.sort_by { |i| i[:duration] }.reverse[0..4].each do |m|
-  puts movie_format(m)
-end
+movies.sort_by { |i| i[:duration] }.last(5).map { |m| puts movie_format(m) }
 
 puts "\n# 10 comedies (first by release date)"
-movies.select { |m| m[:genres].include?('Comedy') }.sort_by { |i| i[:release_at] }[0..9].each do |m|
-  puts movie_format(m)
-end
+movies.select { |m| m[:genres].include?('Comedy') }
+      .sort_by { |i| i[:release_at] }
+      .first(10)
+      .map { |m| puts movie_format(m) }
 
 puts "\n# List of all directors alphabetically (no repetition!)"
 puts(movies.map { |m| m[:director] }.uniq.sort_by { |e| e.split.last })

cc009b2167ea70d1ce162843cc96014bf03fc365
Viktor Kanaev <v.kanaev@potok.digital>
2020-01-17 17:52:27 +0300

    install rubocop-rspec

--- spec/movie_collection_spec.rb
+++ spec/movie_collection_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe MovieCollection do
   end
 
   describe '.new' do
-    subject { described_class.new(file_name) }
+    subject(:new) { described_class.new(file_name) }
 
     let(:file_name) { './spec/fixtures/one_line_movies.txt' }
     let(:movie)     { subject.instance_variable_get(:@movies).first }
@@ -26,9 +26,11 @@ RSpec.describe MovieCollection do
     context 'when all good' do
       it { is_expected.to be_an_instance_of(described_class) }
       its(:file_name) { is_expected.to eq file_name }
+
       it 'creates Array of Movie' do
-        expect(subject.instance_variable_get(:@movies).any? { |e| e.is_a?(Movie) }).to be_truthy
+        expect(new.instance_variable_get(:@movies)).to(be_any { |e| e.is_a?(Movie) })
       end
+
       it 'parse txt file and creates valid Movie' do
         movie_params.each do |key, value|
           expect(movie.send(key)).to eq value
@@ -38,67 +40,75 @@ RSpec.describe MovieCollection do
 
     context 'when wrong number of arguments' do
       subject { described_class.new }
+
       its(:itself) { will raise_error(ArgumentError) }
     end
   end
 
   describe '#all' do
     subject { movie_collection.all }
+
     it { is_expected.to eq instance_variable_movies }
   end
 
   describe '#sort_by' do
-    subject { movie_collection.sort_by(arg) }
+    subject(:all) { movie_collection.sort_by(arg) }
 
     context 'when call with symbol' do
       let(:arg) { :duration }
+
       it 'calls sort_by for instance_variable @movies with arg converted to Proc' do
         expect(instance_variable_movies).to receive(:sort_by) do |*_args, &block|
           expect(arg.to_proc).to eq block
         end
-        subject
+        all
       end
     end
 
     context 'when call with block' do
       let(:arg) { proc { 'hi' } }
-      it '' do
+
+      it 'calls sort_by for instance_variable @movies with given block' do
         expect(instance_variable_movies).to receive(:sort_by) do |*_args, &block|
           expect(arg).to eq block
         end
-        subject
+        all
       end
     end
 
     context 'when call with whong argument type' do
       let(:arg) { 'duration' }
+
       its(:itself) { will raise_error(TypeError) }
     end
   end
 
   describe '#select' do
     context 'when call with hash' do
-      subject { movie_collection.select(country: 'USA') }
+      subject(:list) { movie_collection.select(country: 'USA') }
+
       it 'calls select for instance_variable @movies and pass hash converted to Proc' do
         expect(instance_variable_movies).to receive(:select) do |*_args, &block|
           expect(block).to be_an_instance_of(Proc)
         end
-        subject
+        list
       end
     end
 
     context 'when call with block' do
-      subject { movie_collection.select { |m| m.genre.include?('Comedy') } }
+      subject(:list) { movie_collection.select { |m| m.genre.include?('Comedy') } }
+
       it 'calls select for instance_variable @movies and pass given block' do
         expect(instance_variable_movies).to receive(:select) do |*_args, &block|
           expect(block).to be_an_instance_of(Proc)
         end
-        subject
+        list
       end
     end
 
     context 'when call with whong argument type' do
       subject { movie_collection.select('duration') }
+
       its(:itself) { will raise_error(NoMethodError) }
     end
   end
@@ -106,7 +116,9 @@ RSpec.describe MovieCollection do
   describe '#filter' do
     context 'when call with valid arguments' do
       subject { movie_collection.filter(genre: 'Crime', year: (1993..1995), title: /The Shawshank/i) }
+
       let(:movie) { subject.first }
+
       it 'Select The Shawshank Redemption Movie' do
         movie_params.each do |key, value|
           expect(movie.send(key)).to eq value
@@ -116,6 +128,7 @@ RSpec.describe MovieCollection do
 
     context 'when call with not-existing field' do
       subject { movie_collection.filter(not_existing: 'junk') }
+
       its(:itself) { will raise_error(NoMethodError) }
     end
   end
@@ -123,6 +136,7 @@ RSpec.describe MovieCollection do
   describe '#stats' do
     context 'when call with valid arguments' do
       subject { movie_collection.stats(:director) }
+
       let(:expected_result) do
         {
           'Frank Darabont' => 1,
@@ -131,18 +145,22 @@ RSpec.describe MovieCollection do
           'Sidney Lumet' => 1
         }
       end
+
       it { is_expected.to eq expected_result }
     end
 
     context 'when call with not-existing field' do
       subject { movie_collection.stats(not_existing: 'junk') }
+
       let(:expected_result) { { nil => File.read(file_name).each_line.count } }
+
       it { is_expected.to eq expected_result }
     end
   end
 
   describe '#existing_genres' do
     subject { movie_collection.existing_genres }
+
     it { is_expected.to eq %w[Action Crime Drama] }
   end
 end
--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -29,6 +29,7 @@ RSpec.describe Movie do
     context 'when all good' do
       it { is_expected.to be_an_instance_of(described_class) }
       its(:movie_collection) { is_expected.to eq movie_collection }
+
       it 'creates movie instance with methods from all valid params' do
         params.each do |key, value|
           expect(movie.send(key)).to eq value
@@ -38,11 +39,13 @@ RSpec.describe Movie do
 
     context 'when wrong number of arguments' do
       let(:input) { nil }
+
       its(:itself) { will raise_error(ArgumentError) }
     end
 
     context 'when invalid keys in params' do
       let(:params) { { invalid_key: 'some value', other_invalid_key: 'junk' } }
+
       it 'creates instance without any methods from params' do
         params.each do |key, _value|
           expect { movie.send(key) }.to raise_error(NoMethodError)
@@ -53,6 +56,7 @@ RSpec.describe Movie do
 
   describe '#to_s' do
     subject { movie.to_s }
+
     it { is_expected.to eq 'The Shawshank Redemption (1994-10-14; Crime/Drama) - 142 min' }
   end
 
@@ -63,33 +67,38 @@ RSpec.describe Movie do
 
     context 'when movie has testing genre' do
       let(:testing_genre) { 'Drama' }
+
       it { is_expected.to be_truthy }
     end
 
     context 'when movie has no testing genre' do
       let(:testing_genre) { 'Action' }
+
       it { is_expected.to be_falsey }
     end
 
     context 'when testing genre not present in movie_collection existing_genres' do
       let(:testing_genre) { 'Opera' }
+
       its(:itself) { will raise_error(RuntimeError, err_msg) }
     end
 
     context 'when movie_collection has empty existing_genres' do
       let(:existing_genres) { nil }
       let(:testing_genre)   { 'Opera' }
+
       its(:itself) { will raise_error(NoMethodError) }
     end
 
     context 'when testing genre is nil' do
       let(:testing_genre) { nil }
+
       its(:itself) { will raise_error(RuntimeError, err_msg) }
     end
   end
 
   describe '#matches?' do
-    subject { movie.matches?(field, pattern) }
+    subject(:match) { movie.matches?(field, pattern) }
 
     let(:pattern) { double }
 
@@ -98,7 +107,7 @@ RSpec.describe Movie do
 
       it 'comparing pattern and field value with "===" method' do
         expect(pattern).to receive(:===).with(movie.send(field))
-        subject
+        match
       end
     end
 
@@ -108,12 +117,13 @@ RSpec.describe Movie do
 
       it 'comparing field value and pattern with "any?" method' do
         expect(value).to receive(:any?).with(pattern)
-        subject
+        match
       end
     end
 
     context 'when wrong number of arguments' do
       subject { movie.matches? }
+
       its(:itself) { will raise_error(ArgumentError) }
     end
   end
@@ -124,16 +134,19 @@ RSpec.describe Movie do
     context 'when movie has release_at value' do
       let(:date)   { FFaker::Time.date }
       let(:params) { { release_at: date } }
+
       it { is_expected.to eq date.strftime('%B') }
     end
 
     context 'when movie has not release_at value' do
       let(:params) { {} }
+
       its(:itself) { will raise_error(NoMethodError) }
     end
 
     context 'when wrong number of arguments' do
       subject { movie.month(:some_arg) }
+
       its(:itself) { will raise_error(ArgumentError) }
     end
   end

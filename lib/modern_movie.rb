8e12229f8a1c732ad86caf5829bee3c152b67500
Viktor Kanaev <v.kanaev@potok.digital>
2020-01-23 12:26:12 +0300

    made types of movies

---
+++ lib/modern_movie.rb
@@ -0,0 +1,5 @@
+class ModernMovie < Movie
+  def to_s
+    "#{title} - modern movie: stars #{star_actors.join(', ')}"
+  end
+end
--- spec/new_movie_spec.rb
+++ spec/new_movie_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe NewMovie do
 
     let(:movie) { described_class.new(nil, title: 'Squad: The Enemy Within', year: 2010) }
 
-    Timecop.freeze(Date.new(2015, 1, 15)) do
-      it { is_expected.to eq 'Squad: The Enemy Within - new movie, released 5 years ago!' }
-    end
+    before { Timecop.freeze(Time.local(2015, 9, 1, 10, 5, 0)) }
+
+    it { is_expected.to eq 'Squad: The Enemy Within - new movie, released 5 years ago!' }
   end
 end

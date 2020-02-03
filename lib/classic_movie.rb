f08c513b722d42886af2845b3234ac1904872da8
Viktor Kanaev <v.kanaev@potok.digital>
2020-02-03 15:12:25 +0300

    delete coments

--- lib/classic_movie.rb
+++ lib/classic_movie.rb
@@ -1,6 +1,5 @@
 class ClassicMovie < Movie
   def to_s
-    # binding.pry
     movies_count = movie_collection.filter(director: director).count
     "#{title} - classic movie, director #{director} (his #{movies_count} another films)"
   end

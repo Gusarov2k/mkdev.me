84ddf03d47d83125147fa848283b7125f546d0ef
Viktor Kanaev <v.kanaev@potok.digital>
2020-02-26 14:00:06 +0300

    ByGenre/ByCountry tests

--- lib/movie_industry/by_genre.rb
+++ lib/movie_industry/by_genre.rb
@@ -2,29 +2,14 @@ module MovieIndustry
   class ByGenre
     def initialize(movie_collection)
       @movie_collection = movie_collection
-      @allowing_methods = movie_collection.existing_genres.map { |g| genre_to_sym(g) }.freeze
-    end
-
-    def method_missing(method, *args, &block)
-      if @allowing_methods.include?(method)
-        @movie_collection.filter(genre: sym_to_filter(method))
-      else
-        super
-      end
-    end
-
-    def respond_to_missing?(method, *)
-      @allowing_methods.include?(method) || super
+      movie_collection.existing_genres.each { |g| create_method(g) }
     end
 
     private
 
-    def genre_to_sym(str)
-      str.downcase.gsub('-', '_').to_sym
-    end
-
-    def sym_to_filter(sym)
-      /#{sym.to_s.gsub('_', '-')}/i
+    def create_method(genre)
+      name = genre.downcase.gsub('-', '_').to_sym
+      define_singleton_method(name) { @movie_collection.filter(genre: genre) }
     end
   end
 end
